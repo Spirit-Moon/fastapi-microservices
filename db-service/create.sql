7e774bb15cc0618e96cf10ae45b282e7c4f91c1a
tesnimkh <66539779+tesnimkh@users.noreply.github.com>
2024-01-28 01:50:13 +0100

    Update create.sql

--- db-service/create.sql
+++ db-service/create.sql
@@ -1,1 +1,16 @@
+-- create.sql
 CREATE DATABASE app_db_dev;
+-- Create the teams table
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Create the pokemons table
+CREATE TABLE IF NOT EXISTS pokemons (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE
+);
+
